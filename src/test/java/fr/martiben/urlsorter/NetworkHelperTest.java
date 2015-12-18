84177471ad1f3e127bc038a23f1ec3d2db5b8e81
martiben <dfgd.dfgd@dfg.com>
2015-12-18 15:24:08 +0100

    First step to youtube getter through google API

---
+++ src/test/java/fr/martiben/urlsorter/NetworkHelperTest.java
@@ -0,0 +1,29 @@
+package fr.martiben.urlsorter;
+
+/**
+ * NetworkHelper Test class.
+ * 
+ * @author B-Martinelli
+ */
+public class NetworkHelperTest
+{
+  // /**
+  // * Test the fetch a playlist info.
+  // *
+  // * @throws JSONException
+  // * Error on parsing
+  // * @throws IOException
+  // * Error on communication with remote
+  // */
+  // @Test
+  // public void NetworkHelperTest() throws JSONException, IOException
+  // {
+  // TODO Using Google API
+  // Test for list= PL76oDTZ7imiavuSHTC_tPP4CDbGNgCFq3
+  // String baseUrl = "https://www.googleapis.com/youtube/v3/playlists";
+  // JSONObject jsonRequest = new JSONObject();
+  // jsonRequest.put("part", "PL76oDTZ7imiavuSHTC_tPP4CDbGNgCFq3");
+  //
+  // NetworkHelper.readJsonFromUrl(baseUrl);
+  // }
+}
